692db68a1045edd22d5f6c60a6973541887689aa
Quentin <sonia.pigot@live.fr>
2015-03-06 10:44:28 +0100

    MainController weather normal

--- js/MainController.js
+++ js/MainController.js
@@ -19,8 +19,24 @@ pistApp.controller('MainController', ['$scope', '$http', function($scope, $http)
 			$scope.ready = true;
 	
 	});
-
 	
+	function control(){
+	$http.get('http://kgb.emn.fr:8001/channels/5/feed.json?key=ZSAVTBI11WQOSJWY&results=1')
+
+		.then(function(result) {
+		var feed = result.data.feeds[0];
+		$scope.temp = Math.floor(feed.field1);
+		$scope.hygrometry = Math.floor(feed.field2);
+		$scope.pressure = Math.floor(feed.field3);
+		$scope.wind = Math.floor(feed.field4);
+		$scope.pluviometry = Math.floor(feed.field5);
+		console.log(result);
+			
+	});
+	};
+	
+	$interval(control(), 20000);
+
 	
 }]);
 
