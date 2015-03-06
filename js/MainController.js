edd358d2aa8a3da8c5a5d2ae37d56bb429801809
Sonia <sonia.pigot@live.fr>
2015-03-06 10:10:27 +0100

    dashboard weather
    modif dashboard weather

--- js/MainController.js
+++ js/MainController.js
@@ -2,26 +2,25 @@ pistApp.controller('MainController', ['$scope', '$http', function($scope, $http)
 	var jours = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 	$scope.days = [];
-	var tabTemp = [];
-	$scope.temp = null;
+	$scope.temp = [];
+	$scope.icon = [];
+	$scope.ready = false;
 	
 	$http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=7')
 		.then(function(result) {
 			var list = result.data.list;
 			list.forEach(function(element){
+				$scope.icon.push({image:element.weather[0].icon, alt:element.weather[0].main, title:element.weather[0].description});
 				var dateObject = new Date(element.dt * 1000);
 				var dateReadable = jours[dateObject.getDay()]+' '+dateObject.getDate();
 				$scope.days.push(dateReadable);
-				var tabTemp.push(Math.floor(element.temp.day));
-				
+				$scope.temp.push(Math.floor(element.temp.day));
 			});
+			$scope.ready = true;
 	
+	});
+
 	
-	}
-	$scope.nextDay = function (index) {
-		//$scope.days.push(index);
-		$scope.temp = tabTemp[index];
-		});	
 	
 }]);
 
