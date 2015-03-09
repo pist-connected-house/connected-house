3da00e6653c8dda52fe70e74dce342ea6afa5343
micaelmbagira <micael.mbagira@icloud.com>
2015-03-09 17:36:39 +0100

    indentation

--- js/MainController.js
+++ js/MainController.js
@@ -53,14 +53,13 @@ pistApp.controller('MainController', ['$scope', '$http', "$interval", function($
 
 			.then(function(result) {
 			var feed = result.data.feeds[0];
-        $scope.temp = Math.floor(feed.field2);
-        $scope.humidity = Math.floor(feed.field3);
-		$scope.direction = Math.floor(feed.field7);
-        $scope.speed = Math.floor(feed.field8);
-		$scope.pluviometry = Math.floor(feed.field4);
-        $scope.pressure = Math.floor(feed.field1);
-        $scope.luminosity = Math.floor(feed.field5);
-				
+	        $scope.temp = Math.floor(feed.field2);
+	        $scope.humidity = Math.floor(feed.field3);
+			$scope.direction = Math.floor(feed.field7);
+	        $scope.speed = Math.floor(feed.field8);
+			$scope.pluviometry = Math.floor(feed.field4);
+	        $scope.pressure = Math.floor(feed.field1);
+	        $scope.luminosity = Math.floor(feed.field5);	
 		});
 	}
 	control();
