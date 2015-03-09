705ac9c0e8bd2aa4328b2d71d974688a4b6e80b2
micaelmbagira <micael.mbagira@icloud.com>
2015-03-09 18:03:35 +0100

    Ajout bouton refresh
    Pour forcer le rechargement des données

--- js/weatherCtrl.js
+++ js/weatherCtrl.js
@@ -1,8 +1,7 @@
 pistApp.controller('WeatherController', ['$scope', '$http', "$interval", function($scope, $http, $interval){
-	function control(){
+	$scope.control = function(){
 	$http.get('http://kgb.emn.fr:8001/channels/5/feed.json?key=ZSAVTBI11WQOSJWY&results=1')
-
-		.then(function(result) {
+	.then(function(result) {
 		var feed = result.data.feeds[0];
 		$scope.temp = Math.floor(feed.field2);
 		$scope.humidity = Math.floor(feed.field3);
@@ -12,9 +11,9 @@ pistApp.controller('WeatherController', ['$scope', '$http', "$interval", functio
         $scope.pressure = Math.floor(feed.field1);
         $scope.luminosity = Math.floor(feed.field5);
 			
-	});
+		});
 	};
-	control();
-	$interval(control, 5000);
+	$scope.control();
+	$interval($scope.control, 10000);
 
 }]);
