0f1362ec9292c10b33aa97ca62f49ddc2f5979da
Jérôme <sonia.pigot@live.fr>
2015-03-15 16:34:25 +0100

    Comprend rien

--- js/AdvancedWeatherCtrl.js
+++ js/AdvancedWeatherCtrl.js
@@ -1,7 +1,20 @@
 pistApp.controller('AdvancedWeatherController', ['$scope', '$http', "$interval", function($scope, $http, $interval){
     $scope.control = function(){
-    $scope.dewpoint =Math.floor(3*5);
-    }
+        $http.get('http://kgb.emn.fr:8001/channels/5/feed.json?key=ZSAVTBI11WQOSJWY&results=1')
+	.then(function(result) {
+		var feed = result.data.feeds[0];
+		$scope.temp = parseFloat(feed.field2).toFixed(1);
+		$scope.humidity = Math.floor(feed.field3);
+		$scope.direction = Math.floor(feed.field7);
+        $scope.speed = Math.floor(feed.field8);
+   
+    });
+          };
+    $scope.control2 = function(){
+     $scope.dewpoint =Math.floor(($scope.temp)*5);
+    };
+          
     $scope.control();
+    $scope.control2();
     }]);
     
