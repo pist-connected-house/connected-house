6fbcf4e812b63fd9fdf97102467c50e08e805915
Jérôme <sonia.pigot@live.fr>
2015-03-15 23:30:12 +0100

    Temperature percue formule fausse

--- js/AdvancedWeatherCtrl.js
+++ js/AdvancedWeatherCtrl.js
@@ -8,7 +8,10 @@ pistApp.controller('AdvancedWeatherController', ['$scope', '$http', "$interval",
 		$scope.direction = Math.floor(feed.field7);
         $scope.speed = Math.floor(feed.field8);
             $scope.intermediaire =(17,27*$scope.temp)/(237,7+$scope.temp)+Math.log($scope.humidity);
+            $scope.tempF=Math.floor($scope.temp*1.8+32);
             $scope.dewpoint=parseFloat((237,7*$scope.intermediaire)/(17,27-$scope.intermediaire)).toFixed(1);
+            $scope.intermed=parseFloat(-42,379+2,049*$scope.tempF+10,143*$scope.humidity*100-0.225*$scope.tempF*$scope.humidity*100).toFixed(1);
+            $scope.tempercu=($scope.intermed-32)/1,8;
                                                                              
     });
           };
