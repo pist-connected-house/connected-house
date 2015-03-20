5195edeb9aadb0fc525172127b9ce1b0e32e2542
micaelmbagira <micael.mbagira@icloud.com>
2015-03-20 13:32:50 +0100

    ajout Photovoltaic

--- js/PhotovoltaicController.js
+++ js/PhotovoltaicController.js
@@ -1,37 +1,53 @@
 pistApp.controller('PhotovoltaicController', ['$scope', '$http', '$interval', function($scope, $http, $interval){
+	$scope.errors = false;
+
+	$scope.getPhotoData = function(start, end){
+		if (start > end) { 
+			$scope.errors = true;
+			$scope.message = 'Please enter dates in chronological order';
+		}
+		else
+			$scope.errors = false;
+			//requete http en convertissant les date en timestamp
+	};
+
 
 	//Date picker settings
+	$scope.datepickers = {
+	    startDt: false,
+	    endDt: false
+    };
+
 	$scope.today = function() {
-		$scope.dt = Date.parse(new Date());
+		$scope.startDt = Date.parse(new Date());
+		$scope.endDt = Date.parse(new Date());
   	};
   	$scope.today();
 
 	$scope.clear = function () {
-		$scope.dt = null;
+		$scope.startDt = null;
 	};
 
-	// Disable weekend selection
-	/*$scope.disabled = function(date, mode) {
-	return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-	};*/
 
-	$scope.toggleMin = function() {
+	/*$scope.toggleMin = function() {
 	$scope.minDate = $scope.minDate ? null : new Date();
 	};
-	$scope.toggleMin();
+	$scope.toggleMin();*/
 
-	$scope.open = function($event) {
-	$event.preventDefault();
-	$event.stopPropagation();
+	$scope.open = function($event, which) {
+        $event.preventDefault();
+        $event.stopPropagation();
 
-	$scope.opened = true;
-	};
+        $scope.datepickers[which]= true;
+      };
 
 	$scope.dateOptions = {
 		formatYear: 'yy',
 		startingDay: 1
 	};
 
-	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+	$scope.formats = ['dd/MM/yyyy', 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
 	$scope.format = $scope.formats[0];
+
+	//http://stackoverflow.com/questions/22269964/how-to-use-2-or-more-angular-ui-bootstrap-datepicker-in-1-form
 }]);
